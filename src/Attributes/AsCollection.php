4266952fa6ac5db722fec61a5800594908334753
Androlax2 <theo.benoit16@gmail.com>
2026-02-02 13:25:48 +0100

    Add typed constructor to `AsCollection` attribute for type enforcement

--- src/Attributes/AsCollection.php
+++ src/Attributes/AsCollection.php
@@ -2,7 +2,15 @@
 
 namespace Androlax2\LaravelModelTypedSettings\Attributes;
 
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PARAMETER)]
 class AsCollection
 {
-
+    /**
+     * @param class-string $type
+     */
+    public function __construct(
+        public string $type
+    ) {}
 }
